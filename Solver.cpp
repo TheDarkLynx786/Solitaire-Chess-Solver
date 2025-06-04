5b3fd4991e5c962f973acf2cac8ab62e8896f9cc
Murtaza Haque <murtaza0713@gmail.com>
2025-06-03 23:20:06 -0500

    Added Board Setup

--- Solver.cpp
+++ Solver.cpp
@@ -20,6 +20,88 @@ Solver::Solver(int boardSize) {
 }
 
 void Solver::board_setup() {
+    cout << "Set up the board with the pieces\n\n";
+    
+    // User Input
+    string input;
+    int counter = 1;
+    
+    while(input != "q") {
+        cout << "Piece " << counter << ": " << endl;
+        cout << "Enter board location in algebraic notation (e.g., \"a1\", \"b2\") or 'q' to quit: ";
+        cin >> input;
+        
+        // Exit
+        if (input == "q") { break; }
+        
+        // Invalid Input
+        if (input.length() != 2 || char(input[0]) < 'a' || char(input[0]) > ('a' + board_size - 1) || char(input[1]) < '1' || char(input[1]) > ('1' + board_size - 1)) {
+            cout << "\nInvalid input; enter a valid position in chess algebraic notation (e.g., \"a1\", \"d4\") restricted to your board size." << endl;
+            continue;
+        }
+
+        // Process Input
+        int x = int(char(input[0]) % 'a');
+        int y = int(char(input[1]));
+
+        // Check if position is already occupied        
+        if (board.at(x).at(y) != nullptr) {
+            cout << "Position already occupied!" << endl;
+            continue;
+        }
+
+        // Chess Piece
+        char piece_name;
+        bool invalid_piece = false;
+        cout << "Enter piece type (in algebraic chess notation):\n\nKey:\n"
+             << "K - King\n"
+             << "Q - Queen\n"
+             << "R - Rook\n"
+             << "B - Bishop\n"
+             << "N - Knight\n"
+             << "P - Pawn\n";
+        cout << "---> ";
+        cin >> piece_name;
+        piece_name = toupper(piece_name);
+        
+        string piece_name_str;
+        switch(piece_name) {
+            case 'K':
+                piece_name_str = "King";
+                break;
+            case 'Q':
+                piece_name_str = "Queen";
+                break;
+            case 'R':
+                piece_name_str = "Rook";
+                break;
+            case 'B':
+                piece_name_str = "Bishop";
+                break;
+            case 'N':
+                piece_name_str = "Knight";
+                break;
+            case 'P':
+                piece_name_str = "Pawn";
+                break;
+            default:
+                cout << "Invalid piece type; Enter a valid chess piece from the key." << endl;
+                invalid_piece = true;
+                break;
+        }
+
+        // Invalid Piece
+        if (invalid_piece) { continue; }
+
+        // Make New Piece and Add to Board
+        Piece* piece = new Piece(piece_name);
+        add_piece(piece);
+        place_piece(x, y, piece_name);
+        
+        // Output
+        cout << "\nPlaced a " << piece_name_str << " at " << input << "." << endl;
+        counter++;
+    }
     
 }
 
@@ -39,9 +121,9 @@ void Solver::capture_piece(Piece* piece) {
 }
 
 void Solver::place_piece(int x, int y, char piece_name) {
-    if (board[x][y] != nullptr) {
+    if (board.at(x).at(y) != nullptr) {
         cout << "Position already occupied!" << endl;
         return;
     }
-    board[x][y] = new Piece(piece_name);
+    board.at(x).at(y) = new Piece(piece_name);
 }
